6362c0f7800dce3eefa612ee65fda870c38d17d9
R Dwaraknath <78579652+rdwarakreddy@users.noreply.github.com>
2024-07-09 04:52:47 +0000

    Append at the end

--- DSA/DLL/LinkedList.py
+++ DSA/DLL/LinkedList.py
@@ -4,9 +4,38 @@ class Node:
         self.next = None
         self.prev = None
 
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
     def __str__(self):
-        return f"{self.prev}<--{self.value}-->{self.next}"
+        temp_node = self.head
+        results = ""
+        while temp_node is not None:
+            results += str(temp_node.value)
+            if temp_node.next is not None:
+                results += "<->"
+            temp_node = temp_node.next
+        return results
+
+    def appendAtEnd(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
 
 
-new_node = Node(10)
-print(new_node)
+DLL = DoubleLinkedList()
+DLL.appendAtEnd(1)
+DLL.appendAtEnd(2)
+DLL.appendAtEnd(3)
+
+print(DLL)
