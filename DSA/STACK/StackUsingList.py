e07894f1b20999286458a571363607a9698ef53b
R Dwaraknath <78579652+rdwarakreddy@users.noreply.github.com>
2024-07-10 04:05:35 +0000

    Added Stack using List with out limit

---
+++ DSA/STACK/StackUsingList.py
@@ -0,0 +1,42 @@
+class Stack:
+    def __init__(self):
+        self.list = []
+
+    def __str__(self):
+        values = []
+        for i in reversed(self.list):
+            values.append(str(i))
+            # values.append((i))
+        return "\n".join(values)
+        # return values
+
+    def isEmpty(self):
+        if self.list == []:
+            return True
+        else:
+            return False
+
+    def push(self, value):
+        self.list.append(value)
+        return "The element is inserted"
+
+    def Pop(self):
+        if self.isEmpty():
+            return "There is no element in the stack"
+        else:
+            return self.list.pop()
+
+    def Peek(self):
+        if self.isEmpty():
+            return "There is no element in the stack"
+        else:
+            return self.list[-1]
+
+
+cs = Stack()
+print(cs.isEmpty())
+cs.push(1)
+cs.push(2)
+cs.push(3)
+print(cs)
+print(cs.Peek())
