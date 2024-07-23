7bb987f18197e5203a76ee0ddbd5e964fa0d64f1
sunny <rdwarakreddy@gmail.com>
2024-07-23 21:20:28 +0530

    End of the day

--- LeetcodeQuestions/Top 150 questions/BestTimeToSellBooks.py
+++ LeetcodeQuestions/Top 150 questions/BestTimeToSellBooks.py
@@ -2,10 +2,8 @@ a = [7, 6, 4, 3, 1]
 b = min(a)
 c = a.index(b)
 if len(a) == c + 1:
-    print(0)
+    a.remove(b)
 else:
     n = a[c + 1 :]
     ma = max(n)
     print(ma - b)
-
-print(len(a))  # 6
